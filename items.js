ecb25574d71e33b795651c42f0583d2aa1497e87
Ali <AliHasanJMM@gmail.com>
2024-07-29 00:52:32 +0300

    Items JS

---
+++ items.js
@@ -0,0 +1,20 @@
+const bestOffers = [
+    {
+      name: "Cabinet and Countertop Bundle",
+      imageSrc: "../../images/cabinetbundel.jpg",
+      oldPrice: "BHD1509.26",
+      newPrice: "1056.48",
+    },
+    {
+      name: "Stainless Steel Appliance Package",
+      imageSrc: "../../images/package.jpg",
+      oldPrice: "BHD943.28",
+      newPrice: "754.63",
+    },
+    {
+      name: "Modern Table Set",
+      imageSrc: "../../images/modernTableSet.jpg",
+      oldPrice: "BHD301.47",
+      newPrice: "226.39",
+    },
+  ];
