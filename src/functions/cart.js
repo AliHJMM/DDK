01ef24e95f4eb4661d391412bbdba4dcec170b5e
Ali <AliHasanJMM@gmail.com>
2024-07-29 00:50:02 +0300

    Cart JS

---
+++ src/functions/cart.js
@@ -0,0 +1,8 @@
+let total = 0;
+    let modalBody;
+    let modalFooter;
+
+    $(document).ready(function() {
+      modalBody = $(".cart-modal-body");
+      modalFooter = $(".cart-modal-footer");
+    });
