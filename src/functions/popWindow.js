dc828e42e9832a7d672de3dcd40e3ca0e7f61533
Ali <AliHasanJMM@gmail.com>
2024-07-29 00:59:51 +0300

    PopWindow JS

--- src/functions/popWindow.js
+++ src/functions/popWindow.js
@@ -2,3 +2,11 @@ function closePopup() {
     var popupWindow = document.getElementById('popupWindow');
     popupWindow.style.display = 'none';
 }
+setTimeout(function () {
+    var popupWindow = document.getElementById('popupWindow');
+    popupWindow.style.display = 'block';
+    setTimeout(function () {
+        var popupWindow = document.getElementById('popupWindow');
+        popupWindow.style.display = 'none';
+    }, 15000);
+}, 1000);
